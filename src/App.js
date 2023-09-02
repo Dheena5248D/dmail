c6b5d0df6fe19969731b8cf1455992db04c630e9
dheena5248D <dheenadhayalan5248d@gmail.com>
2023-09-02 16:40:57 +0530

    anmations v1.0

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ return(
   <section id="page1">
   <div className="top_bar">
     <span style={{fontSize:"25px"}}>DD</span>
-    <span style={{display:"flex",justifyContent:"center", marginTop:"0px", top:"0px", fontSize:"25px"}}>Dmail</span>
+    <span id="animated-text" style={{display:"flex",justifyContent:"center", marginTop:"0px", top:"0px", fontSize:"25px",position:"relative" ,left:"1000000"}}>Dmail</span>
     <div style={{display:"flex",justifyContent:"center",marginTop:"40vh",fontSize:"20px"}}><p>Personalize Your ,<a style={{color:"#FFD23F"}}>E</a>mail Experience with Our
  Dynamic <span style={{color:"#FFD23F",marginLeft:"5px"}}> E</span>mail Generator! 📧✨</p></div>
   </div>
   <div style={{display:"flex", justifyContent:"center", alignItems:"end", marginTop:"30vh"}}>
-  <svg style={{fill:"#E57C04"}} xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M246.6 470.6c-12.5 12.5-32.8 12.5-45.3 0l-160-160c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L224 402.7 361.4 265.4c12.5-12.5 32.8-12.5 45.3 0s12.5 32.8 0 45.3l-160 160zm160-352l-160 160c-12.5 12.5-32.8 12.5-45.3 0l-160-160c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L224 210.7 361.4 73.4c12.5-12.5 32.8-12.5 45.3 0s12.5 32.8 0 45.3z"/>  </svg>
-  <span style={{color:"#E57C04", marginLeft:"10px"}}>  scroll down</span>
+  <svg className="bounce-text" style={{fill:"#E57C04"}} xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M246.6 470.6c-12.5 12.5-32.8 12.5-45.3 0l-160-160c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L224 402.7 361.4 265.4c12.5-12.5 32.8-12.5 45.3 0s12.5 32.8 0 45.3l-160 160zm160-352l-160 160c-12.5 12.5-32.8 12.5-45.3 0l-160-160c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L224 210.7 361.4 73.4c12.5-12.5 32.8-12.5 45.3 0s12.5 32.8 0 45.3z"/>  </svg>
+  <span className="bounce-text" style={{color:"#E57C04", marginLeft:"10px"}}>  scroll down</span>
   </div>
   </section>
 )
@@ -40,11 +40,7 @@ return(
       <div className="gridItems"><input style={{width:"90px", height:"40px"}} type="textbox" /></div> 
       <div className="gridItems"><input style={{width:"90px", height:"40px"}} type="textbox" /></div>
       <div className="gridItems"><input style={{width:"190px", height:"40px"}} type="textbox" /></div>
-      <div className="gridItems" style={{textAlign:"center", height:"45px"}}>4</div>
-      <div className="gridItems"><input style={{width:"90px", height:"40px"}} type="textbox" /></div>
-      <div className="gridItems"><input style={{width:"90px", height:"40px"}} type="textbox" /></div> 
-      <div className="gridItems" ><input style={{width:"90px", height:"40px"}}  type="textbox" /></div>
-      <div className="gridItems"><input style={{width:"190px", height:"40px"}} type="textbox" /></div>
+     
     </div>
     <div style={{display:"flex", justifyContent:"center"}}>
       <div id="egtxt" >
